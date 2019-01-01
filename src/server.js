31213357a0a67afb700bbd080249dd4b13aba5e8
Aaron Barack <atbarack@gmail.com>
2018-12-31 20:25:39 -0800

    Added http for devopelopement

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 //Requirments
 const https = require('https'); //protocol the server works on
+const http = require('http'); //for dev use
 const fs = require('fs'); //file system
 const hbs = require('hbs'); //handlebars html templating
 const express = require('express'); //allows to create the web app
@@ -12,7 +13,8 @@ const {mongoose} = require("./database"); //overhead for mongodb
 
 //Configuration
 const app = express(); //create routes
-const server = https.createServer(options, app);
+//const server = https.createServer(options, app); //production
+const server = http.createServer(app); //dev
 server.listen(443); //https
 app.use(cookieParser(secert)); // this needs to put into secerts
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
