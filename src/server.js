f7f190c3ba2dffa6c2c3f8448493c7c1e8092d35
Aaron Barack <atbarack@gmail.com>
2018-12-31 20:16:03 -0800

    Add / route for testing, moved options to secerts

--- src/server.js
+++ src/server.js
@@ -4,17 +4,13 @@ const fs = require('fs'); //file system
 const hbs = require('hbs'); //handlebars html templating
 const express = require('express'); //allows to create the web app
 const bodyParser = require("body-parser"); //parse body req's
-const {secert} = require("./secerts"); //NOT To BE ADDED TO GITHUB
+const {secert, options} = require("./secerts"); //NOT To BE ADDED TO GITHUB
 const cookieParser = require('cookie-parser'); //parse cookies
 const _ = require("lodash"); //nice for formating json
 const {mongoose} = require("./database"); //overhead for mongodb
 
 
 //Configuration
-const options = {
-	 // key: fs.readFileSync('/etc/letsencrypt/live/ammecha.com/privkey.pem'),
-	  //cert: fs.readFileSync('/etc/letsencrypt/live/ammecha.com/fullchain.pem')
-};
 const app = express(); //create routes
 const server = https.createServer(options, app);
 app.use(cookieParser(secert)); // this needs to put into secerts
@@ -24,3 +20,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set("view_engine", "hbs");
+
+//TOOD::CREATE a homepage
+
+app.get("/", (request, response) => {
+  res.send("Hello");
+})
